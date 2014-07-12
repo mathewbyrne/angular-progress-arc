c3c2c9a1917f9ae5f7356a47f75fdf634337f90d
Mathew Byrne <mathewbyrne@gmail.com>
2014-07-12 12:49:38 +1000

    Added grunt tasks.

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> (<%= pkg.homepage %>) */\n'
+            },
+            build: {
+                src: '<%= pkg.name %>.js',
+                dest: '<%= pkg.name %>.min.js'
+            }
+        },
+        jshint: {
+            files: ['<%= pkg.name %>.js']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('build', ['uglify']);
+};
