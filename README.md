f6b677868b1c9b2bc231024eecedff9f02326a13
Mathew Byrne <mathewbyrne@gmail.com>
2014-07-13 20:38:07 +1000

    Implemented simpler method for rendering arc.
    Got the idea from http://css-tricks.com/svg-line-animation-works/ where instead of
    drawing an arc between two points, we're now just drawing a circle element and
    using stroke-dash* properties to give the look we're after.

--- README.md
+++ README.md
@@ -45,7 +45,7 @@ Attributes
 ----------
 
 - `size` defines the pixel width and height for the svg element.  This defaults to __200__.
-- `stroke` defines the color of the progress arc. This should be a valid [stroke property](https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Fills_and_Strokes#Stroke) for an SVG path element.  This defaults to __black__. 
+- `stroke` defines the color of the progress arc. This should be a valid [stroke property](https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Fills_and_Strokes#Stroke) for an SVG circle element.  This defaults to __black__. 
 - `stroke-width` defines the width of the progress arc line in pixels. This should be a value between 1 and `size` / 2. This defaults to __20__.
 - `progress` is an expression that should evaluate to a float value between 0.0 and 1.0; where 0.0 is an empty progress arc, and 1.0 is a full ring.
 - `counter-clockwise` if this attribute is present and set to a "truthy" value (true, yes, 1) the progress bar will render in a counter-clockwise direction. 
@@ -53,17 +53,28 @@ Attributes
 Styling
 =======
 
-As well as using attributes of the directive element, a progress arc can be styled using CSS.
+As well as using attributes of the directive element, a progress arc can be styled using CSS. 
 
 ```css
 // Use a more complex selector in your application to avoid styling other SVG elements.
-path {
+circle {
     stroke: rgba(0, 0, 0, 0.1);
     stroke-linecap: round;
 }
 ```
 
-If you use CSS for styling, try to avoid setting a `stroke-width` property since this is used to calculate the position of the arc inside the SVG element.
+The `stroke-dashoffset` property is used to render the position of the progress arc, so adding a transition for this property will result in smoother transitions between progress values.
+
+```css
+circle {
+    transition: stroke-dashoffset 0.1s linear;
+}
+```
+
+If you use CSS for styling, avoid setting the following properties that are used to calculate the progress arc position:
+- `stroke-width`
+- `stroke-dasharray`
+- `stroke-dashoffset`
 
 License
 =======
--- angular-progress-arc.js
+++ angular-progress-arc.js
@@ -3,32 +3,8 @@
     'use strict';
 
     /**
-     * Converts a polar coordinate into a pair of cartesian points.
-     * @param {number} x
-     * @param {number} y
-     * @param {number} radius
-     * @param {number} radians
-     * @returns {*[]}
-     */
-    var polarToCartesian = function (x, y, radius, radians) {
-        return [
-            x + radius * Math.cos(radians),
-            y + radius * Math.sin(radians)
-        ];
-    };
-
-    /**
-     * Converts a float values between 0 and 1 to radians between 0 and 2π
-     * @param {number} float
-     * @returns {number}
-     */
-    var floatToRadians = function (float) {
-        return (float * Math.PI * 2);
-    };
-
-    /**
-     * Converts a string into a boolean value if the lowercased string equals
-     * a truthy value.
+     * Converts a string into a boolean value if the lowercase string equals a
+     * true-like value.
      * @param {string} input
      * @returns {boolean}
      */
@@ -43,61 +19,6 @@
             }
         }
         return false;
-    };
-
-    var ANGLE_OFFSET = -Math.PI / 2;
-    var ANGLE_MAX = floatToRadians(0.99);
-
-    /**
-     * Generates a string that describes how to draw an SVG arc. Suitable for
-     * binding to the d attribute of an SVG path element.
-     * @param {number,string} size width & height of the drawing area.
-     * @param {number,string} strokeWidth width of the arc.
-     * @param {number} progress value between 0.0 and 1.0 to indicate arc progress.
-     * @param {boolean} counterClockwise set true to draw counter-clockwise
-     * @returns {string}
-     */
-    var getPathDescription = function (size, strokeWidth, progress, counterClockwise) {
-
-        // Cap stroke width so that it's not larger than the radius.
-        size = parseInt(size);
-        strokeWidth = Math.min(parseInt(strokeWidth), size / 2);
-
-        if (progress <= 0.0) {
-            return 'M 0 0'; // Empty string causes error on Chrome.
-        }
-
-        var center = size / 2;
-
-        // -1 from radius to prevent clipping edge of svg element.
-        var radius = (size - strokeWidth) / 2 - 1;
-
-        var startPoint = polarToCartesian(center, center, radius, ANGLE_OFFSET);
-
-        var largeArc = progress > 0.5 ? 1 : 0;
-
-        var angle = progress >= 1.0 ? ANGLE_MAX : floatToRadians(progress);
-        var endPoint = polarToCartesian(
-            center,
-            center,
-            radius,
-            counterClockwise ? ANGLE_OFFSET - angle : ANGLE_OFFSET + angle
-        );
-
-        var d = [
-            'M', startPoint[0], startPoint[1],
-            'A', radius, radius, 0, largeArc, counterClockwise ? 0 : 1, endPoint[0], endPoint[1]
-        ];
-
-        // A complete circle is not possible with a path and a single arc call.
-        // So an additional arc is created here to join back to the initial point.
-        if (progress >= 1.0) {
-            d = d.concat([
-                'A', radius, radius, 0, 0, counterClockwise ? 0 : 1, startPoint[0], startPoint[1]
-            ]);
-        }
-
-        return d.join(' ');
     };
 
     var defaults = {
@@ -127,14 +48,35 @@
                 });
 
                 return function (scope, element, attr) {
-                    scope.getDescription = function () {
-                        return getPathDescription(scope.size, scope.strokeWidth, scope.progress(), resolveBoolean(scope.counterClockwise));
+                    // Firefox has a bug where it doesn't handle rotations and stroke dashes correctly.
+                    // https://bugzilla.mozilla.org/show_bug.cgi?id=949661
+                    scope.offset = /firefox/i.test(navigator.userAgent) ? -89.9 : -90;
+                    var updateRadius = function () {
+                        scope.strokeWidthCapped = Math.min(scope.strokeWidth, scope.size / 2);
+                        scope.radius = (scope.size - scope.strokeWidthCapped) / 2 - 1;
+                        scope.circumference = 2 * Math.PI * scope.radius;
                     };
+                    scope.$watchCollection('[size, strokeWidth]', updateRadius);
+                    scope.$watch('counterClockwise', function () {
+                        if (typeof scope.counterClockwise !== 'boolean') {
+                            scope.counterClockwise = resolveBoolean(scope.counterClockwise);
+                        }
+                    });
+                    updateRadius();
                 };
             },
             template:
                 '<svg ng-attr-width="{{ size }}" ng-attr-height="{{ size }}">' +
-                    '<path fill="none" ng-attr-stroke="{{ stroke }}" ng-attr-stroke-width="{{ strokeWidth }}" ng-attr-d="{{ getDescription() }}">' +
+                    '<circle fill="none" ' +
+                        'ng-attr-cx="{{ size / 2 }}" ' +
+                        'ng-attr-cy="{{ size / 2 }}" ' +
+                        'ng-attr-r="{{ radius }}" ' +
+                        'ng-attr-stroke="{{ stroke }}" ' +
+                        'ng-attr-stroke-width="{{ strokeWidthCapped }}"' +
+                        'ng-attr-stroke-dasharray="{{ circumference }}"' +
+                        'ng-attr-stroke-dashoffset="{{ (counterClockwise ? -1 : 1 ) * (1 - progress()) * circumference }}"' +
+                        'ng-attr-transform="rotate({{ offset }}, {{ size / 2 }}, {{ size / 2 }})"' +
+                        '></circle>' +
                 '</svg>'
         };
     });
