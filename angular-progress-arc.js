f37cbc8ffa7facedef29d2571fbcd5c6da498f30
Michael Robinson <mike@pagesofinterest.net>
2016-02-17 10:39:12 +0800

    Changed compile to link as former was not working in angular 1.4.9

--- angular-progress-arc.js
+++ angular-progress-arc.js
@@ -40,42 +40,39 @@
                 complete:         '&', // Expression evaluating to float [0.0, 1.0]
                 background:       '@'  // Color of the background ring. Defaults to null.
             },
-            compile: function (element, attr) {
+            link: function (scope, element, attr) {
+                progressArcDefaults(scope);
 
-                progressArcDefaults(attr);
-
-                return function (scope, element, attr) {
-                    // Firefox has a bug where it doesn't handle rotations and stroke dashes correctly.
-                    // https://bugzilla.mozilla.org/show_bug.cgi?id=949661
-                    scope.offset = /firefox/i.test(navigator.userAgent) ? -89.9 : -90;
-                    var updateRadius = function () {
-                        scope.strokeWidthCapped = Math.min(scope.strokeWidth, scope.size / 2 - 1);
-                        scope.radius = Math.max((scope.size - scope.strokeWidthCapped) / 2 - 1, 0);
-                        scope.circumference = 2 * Math.PI * scope.radius;
-                    };
-                    scope.$watchCollection('[size, strokeWidth]', updateRadius);
-                    updateRadius();
+                // Firefox has a bug where it doesn't handle rotations and stroke dashes correctly.
+                // https://bugzilla.mozilla.org/show_bug.cgi?id=949661
+                scope.offset = /firefox/i.test(navigator.userAgent) ? -89.9 : -90;
+                var updateRadius = function () {
+                    scope.strokeWidthCapped = Math.min(scope.strokeWidth, scope.size / 2 - 1);
+                    scope.radius = Math.max((scope.size - scope.strokeWidthCapped) / 2 - 1, 0);
+                    scope.circumference = 2 * Math.PI * scope.radius;
                 };
+                scope.$watchCollection('[size, strokeWidth]', updateRadius);
+                updateRadius();
             },
             template:
-                '<svg ng-attr-width="{{size}}" ng-attr-height="{{size}}">' +
-                    '<circle class="ngpa-background" fill="none" ' +
+                '<svg width="{{size}}" height="{{size}}">' +
+                    '<circle fill="none" ' +
                         'ng-if="background" ' +
-                        'ng-attr-cx="{{size/2}}" ' +
-                        'ng-attr-cy="{{size/2}}" ' +
-                        'ng-attr-r="{{radius}}" ' +
-                        'ng-attr-stroke="{{background}}" ' +
-                        'ng-attr-stroke-width="{{strokeWidthCapped}}"' +
+                        'cx="{{size/2}}" ' +
+                        'cy="{{size/2}}" ' +
+                        'r="{{radius}}" ' +
+                        'stroke="{{background}}" ' +
+                        'stroke-width="{{strokeWidthCapped}}"' +
                         '/>' +
-                    '<circle class="ngpa-progress" fill="none" ' +
-                        'ng-attr-cx="{{size/2}}" ' +
-                        'ng-attr-cy="{{size/2}}" ' +
-                        'ng-attr-r="{{radius}}" ' +
-                        'ng-attr-stroke="{{stroke}}" ' +
-                        'ng-attr-stroke-width="{{strokeWidthCapped}}"' +
-                        'ng-attr-stroke-dasharray="{{circumference}}"' +
-                        'ng-attr-stroke-dashoffset="{{(1 - complete()) * circumference}}"' +
-                        'ng-attr-transform="rotate({{offset}}, {{size/2}}, {{size/2}})' +
+                    '<circle fill="none" ' +
+                        'cx="{{size/2}}" ' +
+                        'cy="{{size/2}}" ' +
+                        'r="{{radius}}" ' +
+                        'stroke="{{stroke}}" ' +
+                        'stroke-width="{{strokeWidthCapped}}"' +
+                        'stroke-dasharray="{{circumference}}"' +
+                        'stroke-dashoffset="{{(1 - complete()) * circumference}}"' +
+                        'transform="rotate({{offset}}, {{size/2}}, {{size/2}})' +
                             '{{ (counterClockwise && counterClockwise != \'false\') ? \' translate(0, \' + size + \') scale(1, -1)\' : \'\' }}"' +
                         '/>' +
                 '</svg>'
